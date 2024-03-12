92d84c444e3658e0dce5f7d512283f1f2299ba31
imran-dev100 <iagro7189@gmail.com>
2024-03-12 17:18:31 +0530

    ML-6: restructuring

--- c_gradient_descent/gradient_descent.py
+++ c_gradient_descent/gradient_descent.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.data_processor import load_data
-
+from  b_cost_function.cost_function import compute_cost
 #-------------------------------------------------#
 #                 MAIN SOURCE CODE                #
 #-------------------------------------------------#

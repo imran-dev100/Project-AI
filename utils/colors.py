33dbf26942762ad52030f4eb5150678dccf88003
imran-dev100 <iagro7189@gmail.com>
2024-03-13 12:40:41 +0530

    ML-6: adding colors

---
+++ utils/colors.py
@@ -0,0 +1,3 @@
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0';
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0')
